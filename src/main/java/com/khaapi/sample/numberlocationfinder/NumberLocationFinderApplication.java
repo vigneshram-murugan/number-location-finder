22282d631dfa9b9410f8ef965f181bcfbc96fc44
Murugan, Vigneshram <Vigneshram.Murugan2@T-Mobile.com>
2019-04-26 19:25:03 -0400

    initial commit

---
+++ src/main/java/com/khaapi/sample/numberlocationfinder/NumberLocationFinderApplication.java
@@ -0,0 +1,13 @@
+package com.khaapi.sample.numberlocationfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumberLocationFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumberLocationFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khaapi/sample/numberlocationfinder/NumberLocationFinderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.khaapi.sample.numberlocationfinder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NumberLocationFinderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
